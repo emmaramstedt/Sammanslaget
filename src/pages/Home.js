598ba0b2c7dbb0ca8fe7b324e518e862f85020fa
Deliciaes <71063351+Deliciaes@users.noreply.github.com>
2022-08-24 13:56:44 +0200

    import game files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,18 @@
-const Home = () => {
-  return <>home</>;
-};
+import React from "react";
+import styled from "styled-components";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
+const Wrapper = styled.div`
+  max-width: 500px;
+`;
+
+const Home = () => {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/Build/TheroyOfChangeGZipVersion2.loader.js",
+    dataUrl: "/Build/TheroyOfChangeGZipVersion2.data.unityweb",
+    frameworkUrl: "/Build/TheroyOfChangeGZipVersion2.framework.js.unityweb",
+    codeUrl: "/Build/TheroyOfChangeGZipVersion2.wasm.unityweb",
+  });
+  return <Unity className="atervinnaren" unityProvider={unityProvider} />;
+};
 export default Home;
