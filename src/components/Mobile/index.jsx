c517b5e5442d16c3b2f5848709a451e96c2a22a2
Deliciaes <71063351+Deliciaes@users.noreply.github.com>
2022-08-24 21:29:41 +0200

    logo large

--- src/components/Mobile/index.jsx
+++ src/components/Mobile/index.jsx
@@ -1,24 +1,22 @@
 import styled from "styled-components";
+import atervinnarenlarge from "../../assets/logos/atervinnarenlarge.png";
 
 const MobileView = styled.div`
-  border-style: solid;
-  border-color: tomato;
   border-width: 2px;
   padding: 1em;
-  background-color: #ff00002f;
+  /* background-image: url(${atervinnarenlarge});
+  background-size: cover; */
+  img {
+    width: 100%;
+  }
 `;
 
 const Mobile = () => {
   return (
     <MobileView>
-      <p>
-        ÅterVinnaren kan endast laddas i ett fönster som är minst 1920 pixlar
-        bred.
-      </p>
-      <p>
-        Om du är på en dator och ser detta meddelande, testa då att förstora
-        fönstret!
-      </p>
+      <img src={atervinnarenlarge} />
+      <p>Vänligen besök oss på en dator för att spela ÅterVinnaren.</p>
+      <p>Mobila enheter stöds ej!</p>
     </MobileView>
   );
 };
